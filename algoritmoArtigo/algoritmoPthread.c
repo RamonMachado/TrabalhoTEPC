ea25d88199afcc2b9377d035b91abee0f5d7336c
Rafael Xavier <rafael_ldsx@hotmail.com>
2019-09-22 18:21:27 -0300

    'util.c' para reaproveitamento e remoção de variáveis globais

--- algoritmoArtigo/algoritmoPthread.c
+++ algoritmoArtigo/algoritmoPthread.c
@@ -2,34 +2,30 @@
 #include <time.h>
 #include <pthread.h>
 #include <stdlib.h>
-
-#define STB_IMAGE_IMPLEMENTATION
-#include "../stb_image/stb_image.h"
-#define STB_IMAGE_WRITE_IMPLEMENTATION
-#include "../stb_image/stb_image_write.h"
+#include "util.c"
 
 #define NTHREADS 1
 
-//Variáveis globais para facilitar o código
-int largura, altura, canais;
+typedef struct parametros{
+	int threadId;
+	unsigned char* imagem;
+	int largura;
+	int altura;
+} PARAMETROS;
 
-//Cabeçalho das funções
-unsigned char* carregarImagem(char* nomeImagem, unsigned char *img);
-void salvarImagem(char* nomeImagem, unsigned char *img);
 void *executarAlgoritmo(void *x);
-unsigned char inverterBits(unsigned char pixel);
-
 
 //Main
 int main(void){
 	
 	unsigned char *img;
 	char nomeImagem[300];
+	int largura, altura, canais;
 
 	printf("Digite o nome da imagem PNG (sem .png): ");
 	scanf("%s", nomeImagem);
 
-	img = carregarImagem(nomeImagem, img);
+	img = carregarImagem(nomeImagem, img, &largura, &altura, &canais);
 
 	pthread_t threads[NTHREADS];
   	int rc;
@@ -38,9 +34,15 @@ int main(void){
 	clock_t inicio = clock();
 
   	for(int i = 0; i < NTHREADS; ++i){
-  	  unsigned char* thread_args = img;
-      printf("\n-Iniciando a thread %d\n", i);
-      rc = pthread_create(&threads[i], NULL, executarAlgoritmo, (void *) thread_args);
+  		PARAMETROS thread_args = {
+  			.threadId = i,
+  			.imagem = img,
+  			.largura = largura,
+  			.altura = altura
+  		};
+		//unsigned char* thread_args = img;
+		printf("\n-Iniciando a thread %d\n", i);
+		rc = pthread_create(&threads[i], NULL, executarAlgoritmo, (void *) &thread_args);
     }	
 
     //Aguardando as threads terminarem
@@ -52,36 +54,11 @@ int main(void){
 	clock_t fim = clock();
 	double tempo = ((double) fim - inicio) / CLOCKS_PER_SEC;
 
-	salvarImagem(nomeImagem, img);
+	salvarImagem(nomeImagem, img, largura, altura, canais);
 
 	printf("\n\n-- O algoritmo demorou %f segundos para ser executado.\n\n", tempo);
 
 	return 0;
-}
-
-
-//Função carrega a imagem e armazena no vetor img.
-unsigned char* carregarImagem(char* nomeImagem, unsigned char* img){
-	char caminho[350];
-	sprintf(caminho, "../images/%s.png", nomeImagem);
-	printf("%s\n", caminho);
-    img = stbi_load(caminho, &largura, &altura, &canais, 0);
- 	if(img == NULL) {
- 		printf("Erro ao carregar a imagem\n");
- 		exit(1);
- 	}
-	printf("-- Carregada imagem com largura de %dpx, altura de %dpx e %d canais\n", largura, altura, canais);
-	return img;
-}
-
-//Função salva a imagem nova utilizando o vetor img 
-void salvarImagem(char* nomeImagem, unsigned char *img){
-	char caminho[350];
-	sprintf(caminho, "encripted-algoritmoPthread/%sEncripted.png", nomeImagem);
-	printf("-- Salvando nova imagem como %sEncripted.png\n", nomeImagem);
-	stbi_write_png(caminho, largura, altura, canais, img, largura * canais);
-	stbi_image_free(img);
-	printf("---- Imagem salva com sucesso!\n\n");
 }
 
 //Função executa o algoritmo proposto no artigo
@@ -89,29 +66,20 @@ void *executarAlgoritmo(void *x){
 
 	printf("-- Executando Algoritmo de Encriptação / Desencriptação\n");
 
-	unsigned char *imgPointer = x;
+	PARAMETROS* params = (PARAMETROS*) x;
 
-	for(int i = 0; i < largura*altura; i++){
-		*imgPointer = inverterBits(*imgPointer);
-		imgPointer += 1;
-		*imgPointer = inverterBits(*imgPointer);
-		imgPointer += 1;
-		*imgPointer = inverterBits(*imgPointer);
-		imgPointer += 1;
-		printf("%d\n", i);
+	//unsigned char *imgPointer = x;
+
+	for(int i = 0; i < params->largura * params->altura; i++){
+		*params->imagem = inverterBits(*params->imagem);
+		params->imagem += 1;
+		*params->imagem = inverterBits(*params->imagem);
+		params->imagem += 1;
+		*params->imagem = inverterBits(*params->imagem);
+		params->imagem += 1;
 	}
 
 	printf("---- Algoritmo executado com sucesso!\n\n");
 
 	return NULL;
 }
-
-//Recebe um char e inverte a posição dos 4 MSB com os 4 LSB
-unsigned char inverterBits(unsigned char pixel){
-	unsigned char pixel2 = pixel;
-	pixel = pixel << 4;
-	pixel2 = pixel2 >> 4;
-
-	return pixel + pixel2;
-}
-
--- algoritmoArtigo/algoritmoSequencial.c
+++ algoritmoArtigo/algoritmoSequencial.c
@@ -1,72 +1,37 @@
 #include <stdio.h>
 #include <time.h>
 
-#define STB_IMAGE_IMPLEMENTATION
-#include "../stb_image/stb_image.h"
-#define STB_IMAGE_WRITE_IMPLEMENTATION
-#include "../stb_image/stb_image_write.h"
-
-//Variáveis globais para facilitar o código
-int largura, altura, canais;
-
+#include "util.c"
 
 //Cabeçalho das funções
-unsigned char* carregarImagem(char* nomeImagem, unsigned char *img);
-void salvarImagem(char* nomeImagem, unsigned char *img);
-void executarAlgoritmo(unsigned char *img);
-unsigned char inverterBits(unsigned char pixel);
-
+void executarAlgoritmo(unsigned char *img, int largura, int altura);
 
 //Main
 int main(void){
 
 	unsigned char *img;
+	int largura, altura, canais;
 	char nomeImagem[300];
 
 	printf("Digite o nome da imagem PNG (sem .png): ");
 	scanf("%s", nomeImagem);
 
-	img = carregarImagem(nomeImagem, img);
-	
+	img = carregarImagem(nomeImagem, img, &largura, &altura, &canais);
+
 	clock_t inicio = clock();
-	executarAlgoritmo(img);
+	executarAlgoritmo(img, largura, altura);
 	clock_t fim = clock();
 	double tempo = ((double) fim - inicio) / CLOCKS_PER_SEC;
 
-	salvarImagem(nomeImagem, img);
+	salvarImagem(nomeImagem, img, largura, altura, canais);
 
 	printf("\n\n--O algoritmo demorou %f segundos para ser executado.\n\n", tempo);
 
 	return 0;
 }
 
-
-//Função carrega a imagem e armazena no vetor img.
-unsigned char* carregarImagem(char* nomeImagem, unsigned char* img){
-	char caminho[350];
-	sprintf(caminho, "../images/%s.png", nomeImagem);
-	printf("%s\n", caminho);
-    img = stbi_load(caminho, &largura, &altura, &canais, 0);
- 	if(img == NULL) {
- 		printf("Erro ao carregar a imagem\n");
- 		exit(1);
- 	}
-	printf("-- Carregada imagem com largura de %dpx, altura de %dpx e %d canais\n", largura, altura, canais);
-	return img;
-}
-
-//Função salva a imagem nova utilizando o vetor img 
-void salvarImagem(char* nomeImagem, unsigned char* img){
-	char caminho[350];
-	sprintf(caminho, "encripted-algoritmoSequencial/%sEncripted.png", nomeImagem);
-	printf("-- Salvando nova imagem como %sEncripted.png\n", nomeImagem);
-	stbi_write_png(caminho, largura, altura, canais, img, largura * canais);
-	stbi_image_free(img);
-	printf("---- Imagem salva com sucesso!\n");
-}
-
 //Função executa o algoritmo proposto no artigo
-void executarAlgoritmo(unsigned char* img){
+void executarAlgoritmo(unsigned char* img, int largura, int altura){
 
 	printf("-- Executando Algoritmo de Encriptação / Desencriptação\n");
 
@@ -83,13 +48,3 @@ void executarAlgoritmo(unsigned char* img){
 
 	printf("---- Algoritmo executado com sucesso!\n");
 }
-
-//Recebe um char e inverte a posição dos 4 MSB com os 4 LSB
-unsigned char inverterBits(unsigned char pixel){
-	unsigned char pixel2 = pixel;
-	pixel = pixel << 4;
-	pixel2 = pixel2 >> 4;
-
-	return pixel + pixel2;
-}
-
